e10b7bfef0cca8998144286945874f393118e3e8
Emmanuel GARNIER BOIDUN <manu.boidun@gmail.com>
2024-03-05 14:11:11 +0100

    inscriptionCompte

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Page1 from "./components/Page1";
 import InscriptionCitizen from "./components/InscriptionCitizen";
+import InscriptionCompte from "./components/InscriptionCompte";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Pokedex">
+        <Stack.Screen name="InscriptionCompte" component={InscriptionCompte} />
         <Stack.Screen name="InscriptionCitizen" component={InscriptionCitizen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ components/InscriptionCompte.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Importez le hook de navigation
+import ImageBack from '../assets/hero.png';
+
+const MyAccountScreen = () => {
+  const navigation = useNavigation(); // Initialisez le hook de navigation
+
+  const handleBenevolePress = () => {
+    navigation.navigate('InscriptionCitizen'); // Navigate vers la page Bénévole
+  };
+
+  return (
+    <ImageBackground source={ImageBack} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Mon Compte</Text>
+        <TouchableOpacity style={[styles.button, { backgroundColor: '#FF7F50' }]} onPress={handleBenevolePress}>
+          <Text style={styles.buttonText}>Bénévole</Text>
+        </TouchableOpacity>
+        <Text style={styles.orText}>OU</Text>
+        <TouchableOpacity style={[styles.button, { backgroundColor: '#FF7F50' }]}>
+          <Text style={styles.buttonText}>Entreprise</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#DAAA84',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  orText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
+
+export default MyAccountScreen;
