94002e0bfb25a3b72223b1ca40746221c630311e
Amine FODIL-CHERIF <afodil@ipanipan.com>
2024-03-06 11:52:45 +0100

    finalisation header

--- components/header/HeaderApp.js
+++ components/header/HeaderApp.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, StyleSheet, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from './header_main';
 
-const Header = () => {
+const Header = ({ navigation }) => { // Assurez-vous de passer l'objet navigation en prop si vous utilisez React Navigation
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Fonction pour ouvrir un lien externe
+  const handlePressExternalLink = (url) => {
+    Linking.canOpenURL(url).then((supported) => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open this URL: ${url}`);
+      }
+    });
   };
 
   return (
@@ -28,14 +39,22 @@ const Header = () => {
       </View>
       {isMenuOpen && (
         <View style={styles.menuContainer}>
-          <Text style={styles.menuItem}>Accueil</Text>
-          <Text style={styles.menuItem}>Déconnexion</Text>
-          <Text style={styles.menuItem}>Mentions Légales</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Accueil')}>
+            <Text style={styles.menuItem}>Accueil</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {/* fonction de déconnexion ici */}}>
+            <Text style={styles.menuItem}>Déconnexion</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handlePressExternalLink("https://www.gouvernement.fr/mentions-legales")}>
+            <Text style={styles.menuItem}>Mentions Légales</Text>
+          </TouchableOpacity>
         </View>
       )}
       <View style={styles.separator} />
     </View>
   );
 };
+
+// Vos styles existants ici
 
 export default Header;
